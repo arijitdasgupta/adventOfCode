cc0a90079db83337b6adffc4965a43245a2af048
Arijit Dasgupta <babai@DESKTOP-QLUSNQN>
2023-12-10 01:43:14 +0100

    WIP; cleanup and stuff

--- 2023/day8/src/main.rs
+++ 2023/day8/src/main.rs
@@ -3,7 +3,7 @@ use regex::Regex;
 // (\w{3})\W=\W\((\w{3})\,\W(\w{3})\)
 
 #[derive(Debug)]
-enum Directions {
+enum Direction {
     L,
     R
 }
@@ -14,7 +14,26 @@ struct NetworkUnit {
     r: u32,
 }
 
-fn string_to_u32(s: &str) -> u32 {
+type Network = HashMap<position, NetworkUnit>;
+
+type Directions = Vec<u64, Direction>;
+
+type Position = u32;
+
+struct PositionUnit {
+    position: &Position,
+    direction_index: &u64,
+    directions: &Directions,
+    network: &Network,
+}
+
+impl PositionUnit {
+    fn traverse(self: &Self) -> () {
+        
+    }
+}
+
+fn string_to_u32(s: &str) -> Position {
     let low_chars = s.to_ascii_lowercase();
     let chars = low_chars.as_bytes();
     let [c1, c2, c3] = [chars[0] as u32, chars[1] as u32, chars[2] as u32];
@@ -22,31 +41,9 @@ fn string_to_u32(s: &str) -> u32 {
     return (c3 << 16) + (c2 << 8)+ (c1 << 0);
 }
 
-fn is_last_pos(i: u32, c: u8) -> bool {
+fn is_last_pos(i: Position, c: u8) -> bool {
     let last_byte = (i >> 16) as u8;
     return (last_byte ^ c) == 0 ;
-}
-
-fn does_all_ends_with(positions: &Vec<u32>, ch: &char) -> bool {
-    let low_ch = ch.to_ascii_lowercase();
-    for i in positions.iter() {
-        if !is_last_pos(*i, low_ch as u8) {
-            return false
-        }
-    }
-
-    return true;
-}
-
-enum From {
-    L,
-    R
-}
-
-
-struct InverseNode {
-    node: u32,
-    from_direction: Directions,
 }
 
 fn main() {
@@ -59,12 +56,6 @@ fn main() {
     let mut directions: HashMap<u64, Directions> = HashMap::new();
     let mut direction_size: u64 = 0;
     let mut network: HashMap<u32, NetworkUnit> = HashMap::new();
-    
-    // 26 * 3
-    let _: Vec<NetworkUnit> = Vec::with_capacity(17576);
-
-    assert_eq!(does_all_ends_with(&vec!(string_to_u32("AAZ"),string_to_u32("ASZ"), string_to_u32("FFZ"), string_to_u32("BBZ"), string_to_u32("CCZ")), &'Z'), true);
-    assert_eq!(does_all_ends_with(&vec!(string_to_u32("AAZ"),string_to_u32("AAX"), string_to_u32("FFS")), &'Z'), false);
 
     for line in stdin_handler.lines().into_iter() {
         if let Ok(l) = line {
@@ -84,70 +75,6 @@ fn main() {
                         directions.insert(i.try_into().unwrap(), Directions::R);
                     }
                     direction_size += 1;
-                }
-            }
-        }
-    }
-
-    // Immutable now :-D
-    let direction_size = direction_size;
-    let directions = directions;
-
-    let mut steps: u64 = 0;
-    let mut current_position = string_to_u32("AAA");
-
-    let final_position = string_to_u32("ZZZ");
-
-    while current_position != final_position {
-        let directional_index = steps % direction_size;
-        let direction = directions.get(&directional_index).unwrap();
-
-        let next_position;
-        
-        match direction {
-            Directions::L => next_position = network.get(&current_position).unwrap().l, 
-            Directions::R => next_position = network.get(&current_position).unwrap().r,
-        } 
-        
-        // Updating next position
-        current_position = next_position;
-        steps += 1;
-    }
-
-    println!("Part 1: {}", steps);
-
-    // End of part 1
-
-    let key_iter = network.keys().into_iter();
-
-    let end_positions = Vec::from_iter(
-        key_iter.clone()
-        .filter(|k| { does_all_ends_with(&vec!(**k), &'z') })
-        .map(|x| { *x })
-    );
-
-    let mut inverse_tree: Vec<&mut Vec<Directions>> = Vec::new();
-
-    while true {
-        // Find things that are coming from end_positions
-        for (index, node) in end_positions.iter().enumerate() {
-            // find where i can come from.
-            for network_key in network.keys().into_iter() {
-                let network_value = network.get(network_key).unwrap();
-                if network_value.l == *node {
-                    match inverse_tree.get(index) {
-                        Some(vec) => {
-                            *vec.push(new_value);
-                        },
-                        None => {
-                            let mut vec = Vec::new()
-                            vec.push(new_value);
-    
-                            inverse_tree.
-                        }
-                    }
-                } else if network_value.r == *node {
-                    new_value = Directions::R;
                 }
             }
         }
